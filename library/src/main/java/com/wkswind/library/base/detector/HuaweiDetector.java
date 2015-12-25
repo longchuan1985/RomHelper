27db3a8fc9220601a3caae2fe803394975d9e71b
南风不竞 <weikano@163.com>
2015-12-25 10:54:20 +0800

    prepare for Huawei Emotion UI

---
+++ library/src/main/java/com/wkswind/library/base/detector/HuaweiDetector.java
@@ -0,0 +1,10 @@
+package com.wkswind.library.base.detector;
+
+/**
+ * Created by Administrator on 2015-12-25.
+ */
+public class HuaweiDetector {
+    public boolean isHuawei(){
+        return false;
+    }
+}
